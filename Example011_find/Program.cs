385a97e06dc49cf543919b8818f8c2cbb2e6a85d
«Anastasia» <muzalkova.anastasia@gmail.com>
2022-08-09 07:28:24 +0300

    add Example 011

---
+++ Example011_find/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = { 11, 31, 24, 35, 54, 6, 4, 88 };
+
+int n = array.Length;
+int find = 88;
+
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find)
+    { 
+        Console.WriteLine(index);
+    }
+    index++;
+}
