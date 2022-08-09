4de5b0ed82afb9d12e16a44a8286a243b447beb3
«Anastasia» <muzalkova.anastasia@gmail.com>
2022-08-09 07:32:37 +0300

    add break

---
+++ Example012_array2/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = { 11, 31, 24, 88, 35, 54, 6, 4, 88 };
+
+int n = array.Length;
+int find = 88;
+
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find)
+    { 
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
